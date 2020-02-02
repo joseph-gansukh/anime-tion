2c97bc9c61c789fe05ad4786aa91380c020b3286
joseph-gansukh <g.sukhbat@gmail.com>
2020-02-02 15:44:49 -0600

    Updates

---
+++ precache-manifest.876f601e2c83d005169db240093a7597.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100c29517b1f2f20bb3b0c41e3693365",
+    "url": "/anime-tion/index.html"
+  },
+  {
+    "revision": "12b9c94d40f7ac33c56a",
+    "url": "/anime-tion/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d35f3dbf3de6023b35a8",
+    "url": "/anime-tion/static/js/2.3a17b6f0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/anime-tion/static/js/2.3a17b6f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12b9c94d40f7ac33c56a",
+    "url": "/anime-tion/static/js/main.4743ba2b.chunk.js"
+  },
+  {
+    "revision": "41be5351f10d4aa816f4",
+    "url": "/anime-tion/static/js/runtime-main.83701880.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anime-tion/static/media/logo.5d5d9eef.svg"
+  }
+]);
